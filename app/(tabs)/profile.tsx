f1f622b267dac5db9afa3a9f29fe728c77c9b485
Thiago Taboada <contact.thiago.taboada@gmail.com>
2024-09-02 16:35:36 -0300

    local storage get data

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -5,26 +5,45 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useRouter } from 'expo-router';
 
 const Page = () => {
-  const [username, setUsername] = useState<string | null>(null);
+  const [userId, setUserId] = useState<string | null>(null);
+  const [userName, setUserName] = useState<string | null>(null);
+  const [userCPF, setUserCPF] = useState<string | null>(null);
+  const [userEmail, setUserEmail] = useState<string | null>(null);
   const router = useRouter();
 
   useEffect(() => {
-    const getUsername = async () => {
-      const storedUsername = await AsyncStorage.getItem('username');
-      setUsername(storedUsername);
+    const getUserData = async () => {
+      try {
+        const id = await AsyncStorage.getItem('userId');
+        const name = await AsyncStorage.getItem('userName');
+        const cpf = await AsyncStorage.getItem('userCPF');
+        const email = await AsyncStorage.getItem('userEmail');
+
+        setUserId(id);
+        setUserName(name);
+        setUserCPF(cpf);
+        setUserEmail(email);
+      } catch (error) {
+        console.error('Erro ao recuperar os dados do usuário:', error);
+      }
     };
-    getUsername();
+    getUserData();
   }, []);
 
   const handleLogout = async () => {
-    await AsyncStorage.removeItem('username');
+    await AsyncStorage.removeItem('userId');
+    await AsyncStorage.removeItem('userName');
+    await AsyncStorage.removeItem('userCPF');
+    await AsyncStorage.removeItem('userEmail');
     router.push('/login');
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Profile</Text>
-      {username && <Text style={styles.text}>Hello, {username}!</Text>}
+      {userName && <Text style={styles.text}>Name: {userName}</Text>}
+      {userCPF && <Text style={styles.text}>CPF: {userCPF}</Text>}
+      {userEmail && <Text style={styles.text}>Email: {userEmail}</Text>}
       <Button title="Logout" onPress={handleLogout} />
     </View>
   );
@@ -39,6 +58,7 @@ const styles = StyleSheet.create({
   },
   text: {
     color: Colors.white,
+    marginBottom: 10,
   },
 });
 
