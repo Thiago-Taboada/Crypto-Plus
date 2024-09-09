d7b9537b39714627c94a81a9bef15193e6a1afbb
Thiago Taboada <contact.thiago.taboada@gmail.com>
2024-09-09 17:49:27 -0300

    Update profile.tsx

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,65 +1,30 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
-import React, { useEffect, useState } from 'react';
-import Colors from '@/constants/Colors';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useRouter } from 'expo-router';
+
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Colors from '@/constants/Colors'
+import { Stack } from 'expo-router'
 
 const Page = () => {
-  const [userId, setUserId] = useState<string | null>(null);
-  const [userName, setUserName] = useState<string | null>(null);
-  const [userCPF, setUserCPF] = useState<string | null>(null);
-  const [userEmail, setUserEmail] = useState<string | null>(null);
-  const router = useRouter();
-
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const id = await AsyncStorage.getItem('userId');
-        const name = await AsyncStorage.getItem('userName');
-        const cpf = await AsyncStorage.getItem('userCPF');
-        const email = await AsyncStorage.getItem('userEmail');
-
-        setUserId(id);
-        setUserName(name);
-        setUserCPF(cpf);
-        setUserEmail(email);
-      } catch (error) {
-        console.error('Erro ao recuperar os dados do usuário:', error);
-      }
-    };
-    getUserData();
-  }, []);
-
-  const handleLogout = async () => {
-    await AsyncStorage.removeItem('userId');
-    await AsyncStorage.removeItem('userName');
-    await AsyncStorage.removeItem('userCPF');
-    await AsyncStorage.removeItem('userEmail');
-    router.push('/login');
-  };
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Profile</Text>
-      {userName && <Text style={styles.text}>Name: {userName}</Text>}
-      {userCPF && <Text style={styles.text}>CPF: {userCPF}</Text>}
-      {userEmail && <Text style={styles.text}>Email: {userEmail}</Text>}
-      <Button title="Logout" onPress={handleLogout} />
-    </View>
-  );
-};
+    <>
+      <Stack.Screen options={{ headerShown: false }} />
+      <View style={styles.container}>
+        <Text style={styles.text}>Page2</Text>
+      </View>
+    </>
+  )
+}
+
+export default Page
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     backgroundColor: Colors.black,
   },
   text: {
     color: Colors.white,
-    marginBottom: 10,
   },
-});
-
-export default Page;
+})
