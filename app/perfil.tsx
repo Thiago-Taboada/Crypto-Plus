899ca357f3041d2222281438f7db4096457aa1ae
Thiago Taboada <contact.thiago.taboada@gmail.com>
2024-09-17 16:59:38 -0300

    adicionando configurações

--- app/perfil.tsx
+++ app/perfil.tsx
@@ -1,29 +1,32 @@
-import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
 import React, { useEffect, useState } from 'react';
-import Colors from '@/constants/Colors';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useRouter } from 'expo-router';
+import Colors from '@/constants/Colors';
 import { ChevronLeft } from '@/constants/Icons';
 
 const Page = () => {
-  const [userId, setUserId] = useState<string | null>(null);
   const [userName, setUserName] = useState<string | null>(null);
   const [userCPF, setUserCPF] = useState<string | null>(null);
   const [userEmail, setUserEmail] = useState<string | null>(null);
+  const [userImage, setUserImage] = useState<string | null>(null);
+  const [userPlan, setUserPlan] = useState<string | null>(null);
   const router = useRouter();
 
   useEffect(() => {
     const getUserData = async () => {
       try {
-        const id = await AsyncStorage.getItem('userId');
         const name = await AsyncStorage.getItem('userName');
         const cpf = await AsyncStorage.getItem('userCPF');
         const email = await AsyncStorage.getItem('userEmail');
+        const image = await AsyncStorage.getItem('img64');
+        const plan = await AsyncStorage.getItem('IDplano');
 
-        setUserId(id);
         setUserName(name);
         setUserCPF(cpf);
         setUserEmail(email);
+        setUserImage(image);
+        setUserPlan(plan);
       } catch (error) {
         console.error('Erro ao recuperar os dados do usuário:', error);
       }
@@ -31,53 +34,123 @@ const Page = () => {
     getUserData();
   }, []);
 
-  const handleLogout = async () => {
-    await AsyncStorage.removeItem('userId');
-    await AsyncStorage.removeItem('userName');
-    await AsyncStorage.removeItem('userCPF');
-    await AsyncStorage.removeItem('userEmail');
-    router.push('/login');
+  const handleNavigation = (path: string) => {
+    router.push(path);
   };
 
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
       <View style={styles.header}>
         <TouchableOpacity onPress={() => router.push('/')}>
-            <ChevronLeft width={40} height={40} color={Colors.white} />
+          <ChevronLeft width={40} height={40} color={Colors.white} />
         </TouchableOpacity>
+        <View style={styles.profileInfo}>
+          {userImage && <Image source={{ uri: `data:image/jpeg;base64,${userImage}` }} style={styles.image} />}
+          <View style={styles.textContainer}>
+            {userName && <Text style={styles.name}>{userName}</Text>}
+            {userPlan && <Text style={styles.plan}>Membro {userPlan}</Text>}
+          </View>
+        </View>
       </View>
-      <Text style={styles.text}>Profile</Text>
-      {userName && <Text style={styles.text}>Name: {userName}</Text>}
-      {userCPF && <Text style={styles.text}>CPF: {userCPF}</Text>}
-      {userEmail && <Text style={styles.text}>Email: {userEmail}</Text>}
-      <Button title="Sair" onPress={handleLogout} />
-    </View>
+      <Text style={styles.configTitle}>Configurações</Text>
+      <TouchableOpacity style={styles.option} onPress={() => handleNavigation('/')}>
+        <Text style={styles.optionText}>Dados pessoais</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.option} onPress={() => handleNavigation('/')}>
+        <Text style={styles.optionText}>Email</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.option} onPress={() => handleNavigation('/')}>
+        <Text style={styles.optionText}>Moeda Favorita</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.option} onPress={() => handleNavigation('/')}>
+        <Text style={styles.optionText}>Gerenciar assinatura</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.option} onPress={() => handleNavigation('/')}>
+        <Text style={styles.optionText}>Exportar/Importar dados</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.option} onPress={() => handleNavigation('/')}>
+        <Text style={styles.optionText}>Termos de uso</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.logoutButton} onPress={async () => {
+        await AsyncStorage.removeItem('userId');
+        await AsyncStorage.removeItem('userName');
+        await AsyncStorage.removeItem('userCPF');
+        await AsyncStorage.removeItem('userEmail');
+        await AsyncStorage.removeItem('img64');
+        await AsyncStorage.removeItem('IDplano');
+        router.push('/login');
+      }}>
+        <Text style={styles.logoutText}>Sair</Text>
+      </TouchableOpacity>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexGrow: 1,
     backgroundColor: Colors.black,
+    
   },
   header: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: '100%',
-    paddingTop: 40,
-    paddingBottom: 10,
-    paddingHorizontal: 10,
-    backgroundColor: Colors.gray,
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'flex-start',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: Colors.gray,
   },
-  text: {
+  profileInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexGrow: 1,
+  },
+  image: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    marginRight: 20,
+    marginLeft: 20,
+  },
+  textContainer: {
+    justifyContent: 'center',
+  },
+  name: {
     color: Colors.white,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  plan: {
+    color: 'rgba(255, 255, 255, 0.5)',
+    fontSize: 14,
+  },
+  configTitle: {
+    color: Colors.white,
+    fontSize: 24,
     marginBottom: 10,
+    backgroundColor: Colors.gray,
+    borderBottomWidth: 1,
+    borderBottomColor: Colors.white,
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+  },
+  option: {
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: Colors.gray,
+  },
+  optionText: {
+    color: Colors.white,
+    fontSize: 20,
+  },
+  logoutButton: {
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+  },
+  logoutText: {
+    color: Colors.red,
+    fontSize: 20,
   },
 });
 
