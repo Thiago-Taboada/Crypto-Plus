4ece4d2cab1cfc13d600362fa452b2049f8b9609
Thiago Taboada <contact.thiago.taboada@gmail.com>
2024-09-17 17:08:33 -0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,19 +8,28 @@ import { useRouter } from "expo-router";
 
 const Header = () => {
   const [userName, setUserName] = useState<string | null>(null);
+  const [userImage, setUserImage] = useState<string | null>(null);
   const router = useRouter();
 
   const getUserData = async () => {
     try {
       const name = await AsyncStorage.getItem('userName');
+      const image = await AsyncStorage.getItem('img64');
+
       if (name !== null) {
         setUserName(name);
       } else {
         console.log('Nome do usuário não encontrado.');
         setUserName('Usuário');
       }
+
+      if (image !== null) {
+        setUserImage(image);
+      } else {
+        console.log('Imagem do usuário não encontrada.');
+      }
     } catch (error) {
-      console.error('Erro ao recuperar o nome do usuário:', error);
+      console.error('Erro ao recuperar os dados do usuário:', error);
       setUserName('Usuário');
     }
   };
@@ -33,11 +42,9 @@ const Header = () => {
     <SafeAreaView style={styles.container}>
       <View style={styles.wrapper}>
         <View style={styles.userInfoWrapper}>
-          <TouchableOpacity
-            onPress={() => router.push('/perfil')}
-          >
-            <Image
-              source={{ uri: "DOLPHANANA.png" }}
+          <TouchableOpacity onPress={() => router.push('/perfil')}>
+          <Image
+              source={{ uri: `data:image/jpeg;base64,${userImage}` }}
               style={styles.userImg}
             />
           </TouchableOpacity>
@@ -47,10 +54,7 @@ const Header = () => {
             </Text>
           </View>
         </View>
-        <TouchableOpacity
-          onPress={() => {}}
-          style={styles.btnWrapper}
-        >
+        <TouchableOpacity onPress={() => {}} style={styles.btnWrapper}>
           <Text style={styles.btnText}>
             <BellNotificationIcon width={22} height={22} color={Colors.white} />
           </Text>
@@ -63,7 +67,7 @@ const Header = () => {
 export default Header;
 
 const styles = StyleSheet.create({
-  container: { 
+  container: {
     flex: 1,
     backgroundColor: Colors.black,
   },
@@ -75,14 +79,14 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     backgroundColor: Colors.black,
   },
-  userInfoWrapper: { 
-    flexDirection: "row", 
+  userInfoWrapper: {
+    flexDirection: "row",
     alignItems: "center",
   },
-  userImg: { 
-    height: 50, 
-    width: 50, 
-    borderRadius: 30, 
+  userImg: {
+    height: 50,
+    width: 50,
+    borderRadius: 25,
   },
   userTxtWrapper: {
     marginLeft: 10,
@@ -99,8 +103,8 @@ const styles = StyleSheet.create({
     padding: 8,
     borderRadius: 10,
   },
-  btnText: { 
-    color: Colors.white, 
+  btnText: {
+    color: Colors.white,
     fontSize: 12,
   },
 });
