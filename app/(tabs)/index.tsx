f656203d62f0cf32c8810e74d506b9a77e97be00
Thiago Taboada <contact.thiago.taboada@gmail.com>
2024-09-23 17:06:00 -0300

    novo AuthGuard

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,7 @@
+import React from 'react';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
-import React, { useEffect } from 'react';
-import { useRouter } from 'expo-router';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import Colors from '@/constants/Colors';
 import { PieChart } from 'react-native-gifted-charts';
+import AuthGuard from '@/components/AuthGuard';
 import ExpenseBlock from '@/components/ExpenseBlock';
 import CryptoBlock from '@/components/CryptoBlock';
 import IncomeBlock from '@/components/IncomeBlock';
@@ -12,100 +10,55 @@ import ExpenseList from '@/data/expenses.json';
 import CryptoList from '@/data/cryptos.json';
 import incomeList from '@/data/income.json';
 import spendingList from '@/data/spending.json';
+import Colors from '@/constants/Colors';
 
-const Index = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    const checkLocalStorage = async () => {
-      try {
-        const data = await AsyncStorage.getItem('userId');
-        if (!data) {
-          router.push('/login');
-        }
-      } catch (error) {
-        console.error('Error checking local storage:', error);
-      }
-    };
-
-    checkLocalStorage();
-  }, [router]);
-
+const Index: React.FC = () => {
   const pieData = [
-    {
-      value: 47,
-      color: Colors.tintColor,
-      focused: true,
-      text: '47%',
-    },
-    {
-      value: 40,
-      color: Colors.blue,
-      text: '40%',
-    },
-    {
-      value: 16,
-      color: Colors.white,
-      text: '16%',
-    },
+    { value: 47, color: Colors.tintColor, focused: true, text: '47%' },
+    { value: 40, color: Colors.blue, text: '40%' },
+    { value: 16, color: Colors.white, text: '16%' },
     { value: 3, color: '#FFA5BA', gradientCenterColor: '#FF7F97', text: '3%' },
   ];
 
   return (
-    <View style={[styles.container, { paddingTop: 60 }]}>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-          }}
-        >
-          <View style={{ gap: 10 }}>
-            <Text style={{ color: Colors.white, fontSize: 16 }}>
-              Minhas <Text style={{ fontWeight: '700' }}>Despesas</Text>
-            </Text>
-            <Text style={{ color: Colors.white, fontSize: 36, fontWeight: '700' }}>
-              $1475.<Text style={{ fontSize: 22, fontWeight: '400' }}>00</Text>
-            </Text>
-          </View>
-          <View style={{ paddingVertical: 20, alignItems: 'center' }}>
-            <PieChart
-              data={pieData}
-              donut
-              showGradient
-              sectionAutoFocus
-              semiCircle
-              radius={70}
-              innerRadius={55}
-              innerCircleColor={Colors.black}
-              centerLabelComponent={() => {
-                return (
-                  <View
-                    style={{ justifyContent: 'center', alignItems: 'center' }}
-                  >
-                    <Text
-                      style={{
-                        fontSize: 22,
-                        color: 'white',
-                        fontWeight: 'bold',
-                      }}
-                    >
-                      47%
-                    </Text>
+    <AuthGuard>
+      <View style={[styles.container, { paddingTop: 60 }]}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+            <View style={{ gap: 10 }}>
+              <Text style={{ color: Colors.white, fontSize: 16 }}>
+                Minhas <Text style={{ fontWeight: '700' }}>Despesas</Text>
+              </Text>
+              <Text style={{ color: Colors.white, fontSize: 36, fontWeight: '700' }}>
+                $1475.<Text style={{ fontSize: 22, fontWeight: '400' }}>00</Text>
+              </Text>
+            </View>
+            <View style={{ paddingVertical: 20, alignItems: 'center' }}>
+              <PieChart
+                data={pieData}
+                donut
+                showGradient
+                sectionAutoFocus
+                semiCircle
+                radius={70}
+                innerRadius={55}
+                innerCircleColor={Colors.black}
+                centerLabelComponent={() => (
+                  <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                    <Text style={{ fontSize: 22, color: 'white', fontWeight: 'bold' }}>47%</Text>
                   </View>
-                );
-              }}
-            />
+                )}
+              />
+            </View>
           </View>
-        </View>
 
-        <CryptoBlock cryptoList={CryptoList} />
-        <ExpenseBlock expenseList={ExpenseList} />
-        <IncomeBlock incomeList={incomeList} />
-        <SpendingBlock spendingList={spendingList} />
-      </ScrollView>
-    </View>
+          <CryptoBlock cryptoList={CryptoList} />
+          <ExpenseBlock expenseList={ExpenseList} />
+          <IncomeBlock incomeList={incomeList} />
+          <SpendingBlock spendingList={spendingList} />
+        </ScrollView>
+      </View>
+    </AuthGuard>
   );
 };
 
@@ -116,4 +69,5 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
   },
 });
+
 export default Index;
